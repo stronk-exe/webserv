098f3c485eb4a7e3b51a415715cf3a712088a14d
_stronk <94312066+stronk-exe@users.noreply.github.com>
2023-03-28 17:14:24 +0000

    Let the parsing begins!

--- main.cpp
+++ main.cpp
@@ -1,60 +1,17 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
+#include "webserv.hpp"
 
-#define PORT 8080
-int main()
+int main(int ac, char **av)
 {
-    int server_fd, new_socket; long valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    
-    // Only this line has been changed. Everything is same.
-    char *hello = strdup("HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nwebserv hh!");
-    
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if (ac == 2)
     {
-        perror("In socket");
-        exit(EXIT_FAILURE);
-    }
-    
+        std::fstream file(av[1]);
+        std::string _file, s;
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    
-    memset(address.sin_zero, '\0', sizeof address.sin_zero);
-    
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
-    {
-        perror("In bind");
-        exit(EXIT_FAILURE);
+        while (std::getline(file, s))
+            (_file+=s).push_back('\n');
+        _parser(_file);
     }
-    if (listen(server_fd, 10) < 0)
-    {
-        perror("In listen");
-        exit(EXIT_FAILURE);
-    }
-    while(1)
-    {
-        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-        {
-            perror("In accept");
-            exit(EXIT_FAILURE);
-        }
-        
-        char buffer[30000] = {0};
-        valread = read( new_socket , buffer, 30000);
-        printf("%s\n",buffer );
-        write(new_socket , hello , strlen(hello));
-        printf("------------------Hello message sent-------------------");
-        close(new_socket);
-    }
+    else
+        std::cerr << "invalid number of arguments!" << std::endl;
     return 0;
 }
