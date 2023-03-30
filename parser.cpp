9054c9675ef49f32236ac3b43e474f7ecd8a79eb
_stronk <94312066+stronk-exe@users.noreply.github.com>
2023-03-30 17:20:08 +0000

    parsing the config file

--- parser.cpp
+++ parser.cpp
@@ -1,3 +1,4 @@
+
 #include "webserv.hpp"
 
 typedef struct s_server
@@ -7,11 +8,44 @@ typedef struct s_server
 	struct s_server *next;
 }	t_server;
 
+void _get_ports( t_server *_server, std::string* _servers, int c )
+{
+	for (int i=0; i< c; i++)
+	{
+		std::string port="";
+		int port_index = _servers[i].find("listen");
+		int x=port_index+6;
+		while (_servers[i][x] == ' ')
+			x++;
+		while (_servers[i][x] != ';')
+		{
+			port+=_servers[i][x++];
+			// x++;
+		}
+		_server[i].port = port;
+	}
+}
+
+void _get_hosts( t_server *_server, std::string* _servers, int c )
+{
+	for (int i=0; i< c; i++)
+	{
+		std::string host="";
+		int host_index = _servers[i].find("server_name");
+		int x=host_index+11;
+		while (_servers[i][x] == ' ')
+			x++;
+		while (_servers[i][x] != ';')
+		{
+			host+=_servers[i][x++];
+			// y++;
+		}
+		_server[i].host = host;
+	}
+}
+
 void _parser( std::string s )
 {
-	// std::cout << std::count(s.begin(), s.end(), "server {") << std::endl;
-	// std::cout << s.find("server {") << std::endl;
-	// int i =747;
 	int count = 0;
 	std::string str= "server {";
     size_t nPos = s.find(str, 0); 
@@ -21,21 +55,14 @@ void _parser( std::string s )
 		nPos = s.find(str, nPos + str.size());
 	}
 
-	// number of servers we have
-	std::cout << count << std::endl;
-
-	// std::string _servers[count];
+	std::string _servers[count];
 	int i=0;
-	// int lock=0, c=0;
-	// int temp=0;
+	int c=0;
 
 	while (s[i])
 	{
 		int index = (s.substr(i, s.length())).find("server {")+9;
-		// temp = index;
-		// std::cerr << "indeeeeeeexxxxxxxxx " << s.substr(i, s.length()) << "--------" << std::endl;
 		i += index;
-		// temp = i;
 		while (s[i] && s[i] != '}')
 		{
 			if (s[i] == '{')
@@ -43,40 +70,64 @@ void _parser( std::string s )
 				i++;
 				while (s[i] != '}' && s[i])
 				{
-					std::cout << s[i];
+					_servers[c] += s[i];
 					i++;
 				}
 				i++;
 			}
-			// 	lock = 1;
-			// if (s[index+1] == '}' && lock == 1)
-			// 	lock = 0;
-			// if (s[index] == '}' && lock == 0)
-			// 	lock = 1;
 			else
 			{
-				std::cout << s[i];
+				_servers[c] += s[i];
 				i++;
 			}
-			// i++;
-			// index++;
 		}
-		// if (temp != i)
-		// 	c++;
-		// std::cerr << "fin hna " << s.substr(i+1, s.length()) << std::endl;
-		i++;
-		// break;
-		// std::cerr << "iiiiiiiiiiiiiii " << i << std::endl;
-
+		c++;
+		if (c == count)
+			break;
 	}
 
 
 	// allocating and constracting the servers
-	// t_server _servers[count];
-	// for (int i=0; i<count; i++)
+	t_server _server[count];
+
+	_get_ports(_server, _servers, c);
+	_get_hosts(_server, _servers, c);
+	// for (int i=0; i< c; i++)
 	// {
-	// 	std::cout << s << std::endl;
+	// 	// std::string port="";
+	// 	// int port_index = _servers[i].find("listen");
+	// 	// int x=port_index+6;
+	// 	// while (_servers[i][x] == ' ')
+	// 	// 	x++;
+	// 	// while (_servers[i][x] != ';')
+	// 	// {
+	// 	// 	port+=_servers[i][x];
+	// 	// 	x++;
+	// 	// }
+
+
+	// 	// std::string host="";
+	// 	// int host_index = _servers[i].find("server_name");
+	// 	// int y=host_index+11;
+	// 	// while (_servers[i][y] == ' ')
+	// 	// 	y++;
+	// 	// while (_servers[i][y] != ';')
+	// 	// {
+	// 	// 	host+=_servers[i][y];
+	// 	// 	y++;
+	// 	// }
+		
+	// 	// std::cout << i << " - " << _servers[i]  << std::endl;
+	// 	// _server[i].port = port;
+	// 	_server[i].host = host;
+	// 	// std::cerr << "port " << _server[i].port << std::endl;
+	// 	// std::cerr << "host " << _server[i].host << std::endl;
 	// }
+	for (int i=0; i<count; i++)
+	{
+		std::cout << _server[i].host << std::endl;
+		std::cout << _server[i].port << std::endl;
+	}
 
 	// while (s[i])
 	// {
