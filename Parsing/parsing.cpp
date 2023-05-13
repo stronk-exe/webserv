1d22d1aaf376e0c2ef651a70c272b5a1ea11161b
Mohamed Ait jaouad <mait-jao@e1r1p3.1337.ma>
2023-05-13 15:53:05 +0100

    parsing done

--- Parsing/parsing.cpp
+++ Parsing/parsing.cpp
@@ -1,22 +1,19 @@
 #include "parsing.hpp"
 
-
-
-
-void split(std::vector<std::string> &data, char del1, char del2, char del3, std::string str) 
+void split_conf(std::vector<std::string> &data, std::string str) 
 {
     std::string tmp;
 
     for (size_t i = 0; i < str.size() ; i++)
     {
-        if ((str[i] == del1 || str[i] == del2))
+        if ((str[i] == ' ' || str[i] == '\t'))
         {
             if (!tmp.empty())
                 data.push_back(tmp);
             tmp = "";
             continue;
         }
-        else if (str[i] == del3)
+        else if (str[i] == ';')
         {
             if (!tmp.empty())
                 data.push_back(tmp);
@@ -30,19 +27,15 @@ void split(std::vector<std::string> &data, char del1, char del2, char del3, std:
         data.push_back(tmp);
 }
 
-// void info_server(http_server::parsing &parss, std::vector<std::string>::iterator &it)
-// {
-//     std::vector<std::string>::iterator it;
-//     for (it = parss.data.begin(); it != parss.data.end(); it++)
-//     {
-//         if (*it == "server")
-//             info_server(parss, it);
-//     };
-// }
-
-size_t str_to_num(char *str)
+void error(std::string err)
 {
-    char *p;
+    std::cerr << "\033[1;91m" << err << std::endl;
+    exit(1337);
+}
+
+size_t str_to_num(std::string str)
+{
+    char *_p;
     size_t _int;
     
     _int = std::strtol(str.c_str(), &_p, 10);
@@ -51,12 +44,68 @@ size_t str_to_num(char *str)
     return _int;
 }
 
-void info_err_status(http_server::error_page &error, std::vector<std::string>::iterator &it)
+void info_err_status(std::vector<http_server::error_page> &errors, std::vector<std::string>::iterator &it)
 {
-    for (; it != parss.data.end(); it++);
+    http_server::error_page err;
+
+    for (; * (it + 1) != ";" ; it++)
+        err.error_status.push_back(str_to_num(*it));
+    if (!(*it).empty())
+    {
+        err.path = *it;
+        errors.push_back(err);
+    }
+    else
+        error("location of the error page, not set");
 }
 
-void parss(http_server::parsing &parss)
+void info_(std::vector<std::string>  &vec, std::vector<std::string>::iterator &it)
+{
+    for (; * (it + 1) != ";" ; it++)
+        vec.push_back(*it);
+}
+
+void info_autoindex(http_server::location &loc, std::string &str) 
+{
+    if (str == "on")
+        loc.autoindex = true;
+    else if (str == "off")
+        loc.autoindex = true;
+    else
+        error("autoindex error");
+}
+
+void info_location(std::vector<http_server::location> &locations, std::vector<std::string>::iterator &it)
+{
+    http_server::location loc;
+
+    if (!(*it != "{" && *(it + 1) == "{"))
+        error("location bracket error");
+    loc.name = *it;
+    for (it += 2; *it != "}"; it++)
+    {
+        if (*it == "root" && *(it + 1) != ";" && *(it + 2) == ";")
+            loc.root_location = *(++it);
+        else if (*it == "index")
+            info_(loc.indexs, ++it); 
+        else if (*it == "allow_methods")
+            info_(loc.allows_methods, ++it); 
+        else if (*it == "cgi_pass")
+            info_(loc.cgi_pass, ++it); 
+        else if (*it == "autoindex" && *(it + 1) != ";" && *(it + 2) == ";") 
+            info_autoindex(loc, *(++it));
+        else 
+            error("not understood");
+        it++;
+    }
+    if (*it == "}")
+        locations.push_back(loc);
+    else 
+        error("server bracket");
+    it--;
+}
+
+void parss_info(http_server::parsing &parss)
 {
     http_server::server serv;
     std::vector<std::string>::iterator it;
@@ -65,19 +114,31 @@ void parss(http_server::parsing &parss)
     {
         if (*it == "server" && *(++it) == "{")
         {
-            for (it++; it != parss.data.end(); it++)
+            for (it++; *it != "}"; it++)
             {
-                if (*it == "server_name" && *(it + 1) != ";" && *(it + 2) != ";")
-                    serv.name = (++it);
-                else if (*it == "listen" && *(it + 1) != ";" && *(it + 2) != ";")
+                if (*it == "server_name" && *(it + 1) != ";" && *(it + 2) == ";")
+                    serv.name = *(++it);
+                else if (*it == "root" && *(it + 1) != ";" && *(it + 2) == ";")
+                    serv.root_location = *(++it);
+                else if (*it == "listen" && *(it + 1) != ";" && *(it + 2) == ";")
                     serv.listen_port = str_to_num(*(++it));
                 else if (*it == "error_page")
-                    info_err_status(serv.error);
-                it++
+                    info_err_status(serv.errors, ++it);
+                else if (*it == "index")
+                    info_(serv.indexs, ++it);   
+                else if (*it == "location")
+                    info_location(serv.locations, ++it);
+                else
+                    error("not understood");
+                it++;
             }
+            if (*it == "}")
+                parss.servers.push_back(serv);
+            else 
+                error("server bracket");
         }
         else
-            error();
+            error("not understood");
     }
 }
 
@@ -91,11 +152,8 @@ int main(int ac, char *av[])
         parss.file = av[1];
         std::ifstream file(parss.file);
         while (std::getline(file, str))
-            split(parss.data, ' ', '\t', ';', str);
-        // std::vector<std::string>::iterator it;
-        // for (it = parss.data.begin(); it != parss.data.end(); it++)
-        //     std::cout << *it << std::endl;
-        get_info(parss);
+            split_conf(parss.data, str);
+        parss_info(parss);
         file.close();
     }
     return 0;
