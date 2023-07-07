9452c220638047a0e5b18abfced3658e3f28b3c2
_stronk <94312066+stronk-exe@users.noreply.github.com>
2023-07-07 17:05:01 +0100

    still only with the upload of zmmar.mp4

--- webserv.hpp
+++ webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: mait-jao <mait-jao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 11:15:55 by ael-asri          #+#    #+#             */
-/*   Updated: 2023/05/18 13:25:36 by mait-jao         ###   ########.fr       */
+/*   Updated: 2023/06/11 20:29:24 by mait-jao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,25 @@
 #define WEBSERV_HPP
 
 #include <iostream>
-#include <cstdio>
+#include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <fstream>
-#include <cstdlib>
+#include <map>
+#include <fcntl.h>
+#include <limits.h>
 #include <sstream>
 #include <vector>
-#include <map>
+#include <iterator>
 #include <sys/stat.h>
+#include <map>
+
+
+#include <errno.h>
+
 
 #define _POSIX_SOURCE
 #include <dirent.h>
@@ -33,6 +40,8 @@
 #include <sys/types.h>
 #undef _POSIX_SOURCE
 #include <stdio.h>
+
+extern std::string _webserv_loc;
 
 struct error_page
 {
@@ -43,6 +52,19 @@ struct error_page
 	{
 		this->path = err.path;
 		this->error_status = err.error_status;
+		return *this;
+	}
+};
+
+struct CGI
+{
+    std::string		extension;
+    std::string     path;
+
+	CGI &operator= (const CGI & _cgi)
+	{
+		this->path = _cgi.path;
+		this->extension = _cgi.extension;
 		return *this;
 	}
 };
@@ -63,11 +85,11 @@ struct Redirection
 struct Location
 {
     bool						autoindex;
-    std::string					root_location, name;
+    std::string					root_location, name, uploadDir;
     std::vector<std::string> 	index;
     std::vector<std::string>	allows_methods;
 	Redirection					redirection;
-    std::vector<std::string>	cgi_pass;
+    std::vector<CGI>			cgi_pass;
 
 	Location& operator=(const Location& loc)
 	{
@@ -78,6 +100,7 @@ struct Location
 		this->allows_methods = loc.allows_methods;
 		this->redirection = loc.redirection;
 		this->cgi_pass = loc.cgi_pass;
+		this->uploadDir = loc.uploadDir;
 		return *this;
 	}
 };
@@ -114,64 +137,235 @@ struct Parsing
 
 class Request
 {
-	public:
-		Request() {};
-		~Request() {};
+	public :
 
+		int									fd;
 		std::string							uri;
-		std::string							method;
+		std::string							method, queryString;
 		std::string 						type;
 		int									autoindex;
 		std::string							path;
+		std::map<std::string, std::string>	paths;
 		std::vector<std::string>			index;
 		std::string							root;
 		std::vector<std::string>			redirection;
-		std::vector<std::string>			cgi;
+		std::vector<CGI>					cgi;
+		char								**env;
 		int									client_body_upload;
 		std::map<std::string, std::string>	headers;
 		std::string							body;
 		std::vector<error_page>				error_pages;
+		int									is_method_allowed;
+		std::string							upload_path;
 
 		// Uploads
 		std::string	upload_name;
 		std::string	upload_content_type;
 		std::string	upload_file_name;
 		std::string	upload_data;
+		std::string	boundary;
+
+		Request() {
+			fd = 0;
+			autoindex = 0;
+			client_body_upload = 0;
+			is_method_allowed = 0;
+			
+			env = new char*[11];
+			for (int i = 0; i < 10; i++)
+				env[i] = NULL;
+		};
+		Request(const Request & req) { *this = req;};
+
+		Request& operator=(  const  Request & req) {
+			fd = req.fd;
+			uri = req.uri;
+			method = req.method;
+			type = req.type;
+			autoindex = req.autoindex;
+			path = req.path;
+			paths = req.paths;
+			queryString = req.queryString;
+			index = req.index;
+			root = req.root;
+			redirection = req.redirection;
+			cgi = req.cgi;
+			client_body_upload = req.client_body_upload;
+			headers = req.headers;
+			body = req.body;
+			error_pages = req.error_pages;
+			is_method_allowed = req.is_method_allowed;
+			upload_path = req.upload_path;
+
+		// Uploads
+			upload_name = req.upload_name;
+			upload_content_type = req.upload_content_type;
+			upload_file_name = req.upload_file_name;
+			upload_data = req.upload_data;
+			boundary = req.boundary;
+
+			for (int i = 0; i < 10; i++)
+			{
+				if (env[i])
+					delete env[i];
+				if (req.env[i])
+					env[i] = strdup(req.env[i]);
+				else
+					env[i] = NULL;
+			}
+
+			return *this;
+		};
+
+		~Request() {
+			for (int i = 0; i < 10; i++)
+			{
+				if (env[i])
+					delete env[i];
+			}
+			delete[] env;
+		};
+
 };
 
 class Response
 {
 	public:
-		Response() {};
-		~Response() {};
 
 		int			status;
 		std::string status_message;
-		int			content_length;
+		size_t		content_length;
 		std::string content_type;
 		std::string path;
 		std::string data;
 		std::string body;
+		std::string	location;
+		std::map<std::string, std::string>	mims;
+
+		Response() {
+			status = 0;
+			content_length = 0;
+		};
+		Response(const Response & res) { *this = res; };
+
+		Response& operator=(  const Response & res) { 
+			status = res.status;
+			status_message = res.status_message;
+			content_length = res.content_length;
+			content_type = res.content_type;
+			path = res.path;
+			data = res.data;
+			body = res.body;
+			location = res.location;
+			mims = res.mims;
+			return *this; 
+		};
+
+		~Response() {};
+
 };
 
 
+class Client {
+
+	public:
+
+		int		_id;
+		pid_t	_cgi_pid;
+		bool	_kill_pid;
+		size_t	_wr;
+		int		pipe_fd[2];
+		std::string		file,  body, cookies;
+		int		_read_status, status;
+		int		_write_status, _done_writing;
+		long return_write;
+		int		_done_reading;
+		int		_file_done_reading;
+		int		fd_file, data;
+		int		firstTime_HuH;
+		std::string	buffer, prsing_req;
+		std::string substring, s;
+
+		Request		_request;
+		Response	_response;
+
+		Client( ) {};
+
+		Client(const int id ) {
+			_id = id;
+			_wr = 0;
+			data = 0;
+			_read_status = 1;
+			status = 0;
+			_write_status = 0;
+			_done_reading = 0;
+			_file_done_reading = 0;
+			return_write = 0;
+			firstTime_HuH= 0;
+			_done_writing = 0;
+			fd_file = 0;
+			_cgi_pid = -2;
+			_kill_pid = true;
+		};
+
+		Client(const Client & client) { *this = client; };
+
+		Client& operator= (const Client & client) { 
+			_id = client._id;
+			_wr = client._wr;
+			data = client.data;
+			prsing_req = client.prsing_req;
+			return_write = client.return_write;
+			status = client.status;
+			_read_status = client._read_status;
+			_write_status = client._write_status;
+			_done_reading = client._done_reading;
+			_done_writing = client._done_writing;
+			fd_file = client.fd_file;
+			firstTime_HuH = client.firstTime_HuH;
+			_file_done_reading = client._file_done_reading;
+			_cgi_pid = client._cgi_pid;
+			_kill_pid = client._kill_pid;
+			_request = client._request;
+			body  = client.body;
+			cookies  = client.cookies;
+			_response = client._response;
+			return *this; 
+		};
+		~Client() {};
+
+		bool operator ==(Client &b ) { return _id == b._id; }
+
+
+
+};
+
 // Socket
-void	_socket( Parsing &_server, Request *_request, Response *_response );
+void	_socket( Parsing &_server );
+
+//cgi
+std::string generateRandomString(int length);
+void parent_process(std::string &result, int *pipe_fd);
+void get_body(Client & client);
 
 // Methodes
-void	_get( Response *_response, Request *_request, Server &_server );
-void	_post( Response *_response, Request *_request, Server &_server );
-void	_delete();
+void	_get( Client & _client, Server &_server );
+void	_post( Client & _client, Server &_server );
+void	_delete( Client & _client ,Server &_server );
 
 // CGI
-void	_cgi( Request *_request, Response *_response );
+std::string generateRandomString(int length, std::string ss);
+void	_cgi( Client & _client , Server &_server );
+std::string num_to_str(ssize_t num);
 
 // Request
-void	_request( Parsing &_server, Server &_s, Request *_request, Response *_response, char *s );
+void	_request( Parsing &_server, Server &_s, Request &_request, Response &_response, std::string s );
 
 // Response
-void	_response( Response *_response, Request *_request );
-int		_get_res_body( Request *_request, Response *_response );
+void		_response( Client & _client );
+int			_get_res_body( Client & _client );
+void    	get_indexed_file_data( Request &_request, Response &_response, std::string path );
+std::string	_get_ex( std::string _file_name );
 
 // Utils
 void	print_error(std::string s);
@@ -181,10 +375,10 @@ void	error(std::string err);
 int		str_to_num(std::string str);
 void	parss_info(Parsing &parss);
 void	info_autoindex(Location &loc, std::string &str);
-void	info_(std::vector<std::string>  &vec, std::vector<std::string>::iterator &it);
 void	split_conf(std::vector<std::string> &data, std::string str);
 void	info_err_status(std::vector<error_page> &errors, std::vector<std::string>::iterator &it);
 void	info_location(std::vector<Location> &locations, std::vector<std::string>::iterator &it);
 void	print_data(Parsing &parss);
-
+std::vector<std::string>	info_(std::vector<std::string>::iterator &it);
+int createFile(const char* filename, std::string data);
 #endif
